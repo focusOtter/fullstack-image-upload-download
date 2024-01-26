a506e245bb892fc240bba59a8401936dd308871b
focusotter <mtliendo@focusotter.com>
2024-01-26 11:58:00 -0600

    done after recording

---
+++ _backend/test/yt-s3-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as YtS3Backend from '../lib/yt-s3-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/yt-s3-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new YtS3Backend.YtS3BackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
